120046b9fb686c58ccca15b2a2341a63c7b8f772
inggan <k6551558@gmail.com>
2019-08-01 15:46:27 +0900

    중기청 사업 안드로이드

---
+++ app/src/main/java/com/example/todolistexample/ItemActionListener.java
@@ -0,0 +1,6 @@
+package com.example.todolistexample;
+
+public interface ItemActionListener {
+    boolean onItemMove(int fromPosition, int toPosition);
+    void onItemSwipe(int position, int direction);
+}
